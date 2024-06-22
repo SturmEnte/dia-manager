032f5026db3fbb8173282954a27b2000da68eeb3
Jonas <jonas@sturmente.de>
2024-06-22 20:12:47 +0200

    Set up express

---
+++ src/main.js
@@ -0,0 +1,12 @@
+require("dotenv/config");
+const express = require("express");
+
+const app = express();
+
+app.all("*", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+	console.log("Listening on port " + process.env.PORT);
+});
