15e453a2fc5378a72a1dbc5c5e88d90040c765b6
Jonas <jonas@sturmente.de>
2024-07-15 19:10:56 +0200

    Redirect to login if not logged in

--- src/main.js
+++ src/main.js
@@ -3,10 +3,28 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
+const WHITE_LIST = ["api", "login", "signup"];
+
 const app = express();
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.all("*", (req, res, next) => {
+	for (whiteListWord of WHITE_LIST) {
+		if (req.url.includes(whiteListWord)) {
+			next();
+			return;
+		}
+	}
+
+	if (req.cookies["loggedIn"] == undefined) {
+		res.redirect("/login");
+		return;
+	}
+
+	next();
+});
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "public", "main/main.html"));
