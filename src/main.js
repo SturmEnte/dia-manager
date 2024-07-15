c80514f081fa30b2afc9985f261f2a6e6ffeecb2
Jonas <jonas@sturmente.de>
2024-07-15 20:55:23 +0200

    Async mysql

--- src/main.js
+++ src/main.js
@@ -2,58 +2,70 @@ require("dotenv/config");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const mysql = require("mysql");
+const mysql = require("mysql2/promise");
 
 const WHITE_LIST = ["api", "login", "signup"];
 
-const connection = mysql.createConnection({
-	host: process.env.MYSQL_HOST,
-	user: process.env.MYSQL_USER,
-	password: process.env.MYSQL_PASSWORD,
-});
+// connection.connect((err) => {
+// 	if (err) {
+// 		console.error("Error while connecting to database");
+// 		throw err;
+// 	}
 
-connection.connect((err) => {
-	if (err) {
+// 	console.log("Connected to database");
+// });
+
+(async () => {
+	// Connect to database
+	let connection;
+	try {
+		connection = await mysql.createConnection({
+			host: process.env.MYSQL_HOST,
+			user: process.env.MYSQL_USER,
+			password: process.env.MYSQL_PASSWORD,
+			database: process.env.MYSQL_DATABASE,
+		});
+	} catch (error) {
 		console.error("Error while connecting to database");
-		throw err;
+		throw error;
 	}
 
-	console.log("Connected to database");
-});
+	console.log(await connection.execute("SELECT * FROM test"));
 
-const app = express();
+	const app = express();
 
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+	app.use(cookieParser());
+	app.use(express.static(path.join(__dirname, "public")));
 
-app.all("*", (req, res, next) => {
-	for (whiteListWord of WHITE_LIST) {
-		if (req.url.includes(whiteListWord)) {
-			next();
+	app.all("*", (req, res, next) => {
+		for (whiteListWord of WHITE_LIST) {
+			if (req.url.includes(whiteListWord)) {
+				next();
+				return;
+			}
+		}
+
+		if (req.cookies["loggedIn"] == undefined) {
+			res.redirect("/login");
 			return;
 		}
-	}
 
-	if (req.cookies["loggedIn"] == undefined) {
-		res.redirect("/login");
-		return;
-	}
+		next();
+	});
 
-	next();
-});
+	app.get("/", (req, res) => {
+		res.sendFile(path.join(__dirname, "public", "main/main.html"));
+	});
 
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "public", "main/main.html"));
-});
+	app.get("/login", (req, res) => {
+		res.sendFile(path.join(__dirname, "public", "login/login.html"));
+	});
 
-app.get("/login", (req, res) => {
-	res.sendFile(path.join(__dirname, "public", "login/login.html"));
-});
+	app.get("/signup", (req, res) => {
+		res.sendFile(path.join(__dirname, "public", "signup/signup.html"));
+	});
 
-app.get("/signup", (req, res) => {
-	res.sendFile(path.join(__dirname, "public", "signup/signup.html"));
-});
-
-app.listen(process.env.PORT, () => {
-	console.log("Listening on port " + process.env.PORT);
-});
+	app.listen(process.env.PORT, () => {
+		console.log("Listening on port " + process.env.PORT);
+	});
+})();
