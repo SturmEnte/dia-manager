78bb619a44dae17da57f5ced6f1ec6c11dffb4d7
Jonas <jonas@sturmente.de>
2024-07-16 16:36:54 +0200

    Check for max username and password length

--- src/routes/auth/signup.ts
+++ src/routes/auth/signup.ts
@@ -4,6 +4,8 @@ import { Client } from "pg";
 import formatDate from "../../util/formatDate";
 import { hashString } from "../../util/hashing";
 
+const userCredentials = require("../../../configs/usercredentials.json");
+
 let client: Client | undefined;
 
 const router = Router();
@@ -27,7 +29,19 @@ router.post("/signup", async (req, res) => {
 	}
 
 	const username: string = req.body.username;
-	const password: string = hashString(req.body.password);
+	const password: string = req.body.password;
+
+	if (username.length > userCredentials.maxUsernameLength) {
+		res.status(400).json({ error: "The username is too long" });
+		return;
+	}
+
+	if (password.length > userCredentials.maxPasswordLength) {
+		res.status(400).json({ error: "The password is too long" });
+		return;
+	}
+
+	const hashedPassword: string = hashString(password);
 
 	if ((await client.query(`SELECT COUNT(id) FROM users WHERE LOWER(username) = LOWER('${username}')`)).rows[0].count > 0) {
 		res.status(400).json({ error: "A user with that username already exists" });
@@ -35,7 +49,7 @@ router.post("/signup", async (req, res) => {
 	}
 
 	try {
-		await client.query(`INSERT INTO users (username, password, created) VALUES ('${username}', '${password}', '${formatDate(new Date())}')`);
+		await client.query(`INSERT INTO users (username, password, created) VALUES ('${username}', '${hashedPassword}', '${formatDate(new Date())}')`);
 		res.json({ message: "Created user" });
 	} catch (error) {
 		if (error) {
