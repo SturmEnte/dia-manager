02ce13401842bbe13ba1d10a085e5e94b0ec5c31
Jonas <jonas@sturmente.de>
2024-07-17 18:30:24 +0200

    Made registering page working

---
+++ src/public/signup/signup.js
@@ -0,0 +1,32 @@
+const usernameElement = document.getElementById("username");
+const passwordElement = document.getElementById("password");
+const errorElement = document.getElementById("error");
+
+document.getElementById("login-button").onclick = () => {
+	fetch("/api/auth/signup", {
+		method: "post",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			username: usernameElement.value,
+			password: passwordElement.value,
+		}),
+	}).then(async (res) => {
+		if (!res.headers.has("Content-Type") || !res.headers.get("Content-Type").includes("application/json")) {
+			errorElement.innerText = "Error in server response";
+			return;
+		}
+
+		const body = await res.json();
+
+		if (body.error) {
+			errorElement.innerText = "Error: " + body.error;
+			return;
+		}
+
+		errorElement.innerText = body.message;
+		errorElement.style.color = "green";
+		window.location.href = "/login";
+	});
+};
