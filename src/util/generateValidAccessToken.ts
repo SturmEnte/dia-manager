fcb4eb7a4ca03e89ed43d42b270feb9810b213c8
Jonas <jonas@sturmente.de>
2024-07-17 20:15:45 +0200

    Fixed access token generator

--- src/util/generateValidAccessToken.ts
+++ src/util/generateValidAccessToken.ts
@@ -2,16 +2,25 @@ import { Client } from "pg";
 import { randomBytes } from "crypto";
 
 const tablesConfig = require("../../configs/tables.json");
+const generationConfig = require("../../configs/generation.json");
 
 // Generate a random token and check if it is already in use
 // If an error occurs during the genration or checking, then "undefined" is returned else a valid token is returned
 export default async function (client: Client): Promise<string | undefined> {
 	let token: string;
 
+	let start = Date.now();
+
 	try {
 		do {
-			token = randomBytes(tablesConfig.users.tokenLength).toString();
-		} while (await client.query(`SELECT COUNT(id) FROM access_tokens WHERE token = '${token}'`));
+			// Timeout the generation process
+			if (Date.now() - start > generationConfig.password.timeoutMillis) {
+				return undefined;
+			}
+
+			// Generate token
+			token = randomBytes(Math.floor(tablesConfig.users.tokenLength / 2)).toString("hex");
+		} while ((await client.query(`SELECT COUNT(id) FROM access_tokens WHERE token = '${token}'`)).rows[0].count > 0);
 	} catch (error) {
 		console.error("Error while generating access token");
 		console.error(error);
