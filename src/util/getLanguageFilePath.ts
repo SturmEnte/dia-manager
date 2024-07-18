9787bf4d688574af24087872a364e59425101c16
SturmEnte <sturmente@proton.me>
2024-07-18 10:54:24 +0200

    Removed useless console log

--- src/util/getLanguageFilePath.ts
+++ src/util/getLanguageFilePath.ts
@@ -3,8 +3,6 @@ import { existsSync } from "fs";
 export default function (path: string, language: string): string {
 	const languagePath = path.replace("html", language + ".html");
 
-	console.log(languagePath);
-
 	if (existsSync(languagePath)) {
 		return languagePath;
 	}
