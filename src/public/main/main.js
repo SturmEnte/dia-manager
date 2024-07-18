7dbd48583639f6d5862d9a76ce8e924b5520129a
Jonas <jonas@sturmente.de>
2024-07-18 22:44:35 +0200

    Made product selector work

--- src/public/main/main.js
+++ src/public/main/main.js
@@ -1,9 +1,11 @@
 (async () => {
 	const ENTRY_ATTRIBUTE_IDS = ["name", "ref", "lot", "date-of-manufacture", "expiry-date", "start-of-use", "end-of-use"];
 
+	// Dictionary manager
 	const dictionaryManager = new DictionaryManager(getCookieValue("language"));
 	await dictionaryManager.init();
 
+	// Language selector
 	const languageSelector = new LanguageSelector(document.getElementById("language-selector"));
 	languageSelector.addLanguageChangeListener(async (language) => {
 		// Preload the language to the local storage
@@ -13,6 +15,33 @@
 		location.reload();
 	});
 
+	// Product selector
+	const productSelector = document.getElementById("product-selector");
+	const productsContainerElement = document.getElementById("products");
+
+	let products = [];
+
+	for (productElement of productsContainerElement.children) {
+		if (productElement.id != productSelector.value) {
+			productElement.hidden = true;
+		}
+		products.push({ id: productElement.id, element: productElement });
+	}
+
+	productSelector.oninput = () => {
+		for (product of products) {
+			console.log(product);
+
+			if (product.id != productSelector.value) {
+				product.element.hidden = true;
+				console.log("hide");
+				continue;
+			}
+			product.element.hidden = false;
+		}
+	};
+
+	//Functions
 	function generateEntry(data) {
 		let element = document.createElement("div");
 		element.classList.add("entry");
