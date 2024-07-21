76bf86dbb6db0e3616a2ea89386fa569ee7ae5e7
Jonas <jonas@sturmente.de>
2024-07-22 00:07:48 +0200

    Added back basic looks of main page

--- frontend/src/pages/Main.svelte
+++ frontend/src/pages/Main.svelte
@@ -1,1 +1,128 @@
-<main>Main</main>
+<main>
+	<div id="header">
+		<div id="main-title">Dia Manager</div>
+		<div id="menu">
+			<select id="product-selector">
+				<option value="catheters">Catheters</option>
+				<option value="sensors">Sensors</option>
+			</select>
+			<select id="language-selector"></select>
+			<a href="/logout" id="logout-link">
+				<img src="/icons/material/logout-black.svg" alt="Logout" />
+			</a>
+		</div>
+	</div>
+
+	<div id="products">
+		<div id="catheters" class="product">Catheters</div>
+		<div id="sensors" class="product">Sensors</div>
+	</div>
+</main>
+
+<style>
+	:root {
+		--margin: 3vw;
+		--title-size: 3vw;
+	}
+
+	main {
+		background: var(--background);
+		padding: var(--margin);
+		box-sizing: border-box;
+		width: 100vw;
+		height: 100vh;
+	}
+
+	#header {
+		width: 100%;
+		margin-bottom: calc(var(--margin) / 2);
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	#main-title {
+		font-size: var(--title-size);
+		color: var(--primary);
+	}
+
+	#main-title:after {
+		content: "";
+		display: block;
+		width: 100%;
+		height: 1px;
+		background-color: black;
+		position: relative;
+		bottom: 0;
+		left: 0;
+	}
+
+	#menu {
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+		height: 100%;
+	}
+
+	#menu select,
+	#menu a {
+		margin-left: 0.8vw;
+	}
+
+	#language-selector,
+	#logout-link,
+	#product-selector {
+		background: var(--secondary);
+		height: var(--title-size);
+	}
+
+	#language-selector,
+	#logout-link {
+		width: var(--title-size);
+
+		border-radius: 100%;
+		text-align: center;
+		cursor: pointer;
+	}
+
+	#language-selector {
+		-webkit-appearance: none;
+		-moz-appearance: none;
+		appearance: none;
+		border: none;
+		font-size: calc(var(--title-size) * 0.6);
+	}
+
+	#product-selector {
+		border-radius: 0.8vw;
+		border: none;
+		font-size: calc(var(--title-size) * 0.5);
+	}
+
+	#logout-link {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	#logout-link img {
+		width: calc(var(--title-size) * 0.8);
+		height: calc(var(--title-size) * 0.8);
+	}
+
+	#products {
+		background: blue;
+	}
+
+	.element {
+		border: solid black 1px;
+	}
+
+	.section .title {
+		font-size: 1.5rem;
+	}
+
+	.entry {
+		border: solid black 1px;
+	}
+</style>
