dd91f3ba6c8e45968069c71734ce76fe9be9275f
Jonas <jonas@sturmente.de>
2024-07-25 21:02:31 +0200

    Rewrite of the login file
    I restructured the code, added SQL injection prevention and added comments

--- server/src/routes/api/auth/login.ts
+++ server/src/routes/api/auth/login.ts
@@ -12,74 +12,71 @@ const router = Router();
 router.use(json());
 
 router.post("/login", async (req, res) => {
+   // Check if the database connection client was successfully given when creating the router
+   // If not, then the server will respond with an error
+   // After that an error will be thrown that will lead to the server crashing
+   // If the server is setup correctly, it will just restart the server and hopefully resolve this issue by restarting it
    if (!client) {
       res.status(500).json({ error: "Failed database connection" });
       return;
    }
 
-   if (!req.body.username) {
-      res.status(400).json({ error: "Username required" });
+   const { username, password } = req.body;
+
+   // Check if the username and password are both given and strings
+   // The username and password should generally be strings, but this prevents unnecessary crashes
+   if (!username || typeof username !== "string") {
+      res.status(400).json({ error: "Username required and must be a string" });
       return;
    }
 
-   if (!req.body.password) {
-      res.status(400).json({ error: "Password required" });
+   if (!password || typeof password !== "string") {
+      res.status(400).json({ error: "Password required and must be a string" });
       return;
    }
 
-   const username: string = req.body.username;
-   const password: string = req.body.password;
-
-   if ((await client.query(`SELECT COUNT(id) FROM users WHERE LOWER(username) = LOWER('${username}')`)).rows[0].count < 1) {
-      res.status(400).json({ error: "No user with that username exists" });
-      return;
-   }
-
-   let id: string;
-
-   // Check if the entered password matches the password stored in the database
    try {
-      const user = (await client.query(`SELECT id, password FROM users WHERE username = '${username}'`)).rows[0];
-      id = user.id;
+      // Check if there is a user with the given username (ignoring casing)
+      // If not, then return with an user error
+      const userQuery = await client.query("SELECT id, password FROM users WHERE LOWER(username) = LOWER($1)", [username]);
+      if (userQuery.rows.length === 0) {
+         res.status(400).json({ error: "No user with that username exists" });
+         return;
+      }
+
+      // Retrive the user data from the database's response
+      const user = userQuery.rows[0];
       const savedHashedPassword = user.password;
       const hashedPassword = hashString(password);
 
-      if (savedHashedPassword != hashedPassword) {
+      // Check if the given password and saved password match
+      // If not, then an user error will be returned
+      if (savedHashedPassword !== hashedPassword) {
          res.status(400).json({ error: "The password is incorrect" });
          return;
       }
-   } catch (error) {
-      if (error) {
-         console.error("Error while checking user credentials");
-         console.error(error);
-         res.status(500).json({ error: "Error while checking user credentials" });
-         return;
-      }
-   }
 
-   // Create access token
-   try {
+      // Generate access token that is not used yet
+      // If the function takes to long, it will return undefined
+      // This will lead to the login process to fail and an error to be thrown
       const accessToken = await generateValidAccessToken(client);
-
       if (!accessToken) {
-         console.error("Error while generating a valid access token");
-         return;
+         throw new Error("Error while generating a valid access token");
       }
 
-      await client.query(`INSERT INTO access_tokens (user_id, token, created) VALUES (${id}, '${accessToken}','${formatDate(new Date())}')`);
+      // Insert the newly generated access token with the user id and creation time/date into the database
+      await client.query(`INSERT INTO access_tokens (user_id, token, created) VALUES ($1, $2, $3)`, [user.id, accessToken, formatDate(new Date())]);
 
+      // Respond with the new access token, if the processes before were successfull
       res.json({ token: accessToken });
-      return;
    } catch (error) {
-      if (error) {
-         console.error("Error while creating access token");
-         console.error(error);
-         res.status(500).json({ error: "Error while creating access token" });
-         return;
-      }
+      // Log the error and respond to the client with an internal server error message if an error occurres
+      console.error("Error during login process", error);
+      res.status(500).json({ error: "Internal server error" });
    }
 });
 
+// Get the databse connection client instance when creating the route
 export default (newClient: Client): Router => {
    client = newClient;
    return router;
