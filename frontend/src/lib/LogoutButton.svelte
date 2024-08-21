f4d0f77d380dbbad508f8e430b194c093ad26bbb
Jonas <jonas@sturmente.de>
2024-08-21 22:03:48 +0200

    Moved logout button to seperate component

---
+++ frontend/src/lib/LogoutButton.svelte
@@ -0,0 +1,28 @@
+<script>
+   import LogoutIconBlack from "../assets/icons/material/logout-black.svg";
+</script>
+
+<div>
+   <a href="/logout" id="logout-link">
+      <img src={LogoutIconBlack} alt="Logout" />
+   </a>
+</div>
+
+<style>
+   #logout-link {
+      background: var(--secondary);
+      width: var(--title-size);
+      height: var(--title-size);
+      border-radius: 100%;
+      text-align: center;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+   }
+
+   img {
+      width: calc(var(--title-size) * 0.8);
+      height: calc(var(--title-size) * 0.8);
+   }
+</style>
--- frontend/src/pages/Main.svelte
+++ frontend/src/pages/Main.svelte
@@ -1,8 +1,8 @@
 <script>
-   import LogoutIconBlack from "../assets/icons/material/logout-black.svg";
-
    import LanguageSelector from "../lib/LanguageSelector.svelte";
    import Product from "../lib/Product.svelte";
+
+   import LogoutButton from "../lib/LogoutButton.svelte";
 
    export let dictionaryManager;
 
@@ -49,9 +49,9 @@
          <div>
             <LanguageSelector />
          </div>
-         <a href="/logout" id="logout-link">
-            <img src={LogoutIconBlack} alt="Logout" />
-         </a>
+         <div>
+            <LogoutButton />
+         </div>
       </div>
    </div>
 
@@ -112,40 +112,19 @@
    }
 
    #menu select,
-   #menu a,
    #menu div {
       margin-left: 0.8vw;
    }
 
-   #logout-link,
    #product-selector {
       background: var(--secondary);
       height: var(--title-size);
-   }
-
-   #logout-link {
-      width: var(--title-size);
-
-      border-radius: 100%;
-      text-align: center;
-      cursor: pointer;
    }
 
    #product-selector {
       border-radius: 0.8vw;
       border: none;
       font-size: calc(var(--title-size) * 0.5);
-   }
-
-   #logout-link {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-   }
-
-   #logout-link img {
-      width: calc(var(--title-size) * 0.8);
-      height: calc(var(--title-size) * 0.8);
    }
 
    #products {
