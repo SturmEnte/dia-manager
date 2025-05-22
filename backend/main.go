1423dcfae00f0c4c0d60895f77ab974c4b0af5f4
SturmEnte <sturmente@proton.me>
2025-05-22 11:04:53 +0200

    feat: Automaticly delete sessions when they expire

--- backend/main.go
+++ backend/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"dia-manager-backend/config"
 	"dia-manager-backend/router"
+	"dia-manager-backend/scheduler"
 )
 
 func main() {
     cfg := config.Load()
-
     config.ConnectPostgres(cfg.DatabaseUri)
 
-    r := router.SetupRouter(cfg)
+    scheduler.StartScheduler()
 
     r.Run(":" + cfg.ServerPort)
 }
---
+++ backend/scheduler/db.go
@@ -0,0 +1,34 @@
+package scheduler
+
+import (
+	"context"
+	"dia-manager-backend/config"
+	"log"
+
+	"github.com/robfig/cron/v3"
+)
+
+func StartScheduler() {
+    c := cron.New()
+
+    _, err := c.AddFunc("@every 1h", func() {
+        log.Println("Running cron job...")
+        runSQLTask()
+    })
+
+    if err != nil {
+        log.Fatalln("Error adding cron job: " + err.Error())
+    }
+
+    c.Start()
+}
+
+func runSQLTask() {
+    _, err := config.DB.Exec(context.Background(), `DELETE FROM sessions WHERE expires < NOW()`)
+
+    if err != nil {
+        log.Println("SQL task failed: " + err.Error())
+    } else {
+        log.Println("SQL task executed successfully. Deleted expired sessions")
+    }
+}
