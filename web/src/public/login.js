119a436a98812c0d3132951267265699be088e15
Jonas <jonas@sturmente.de>
2025-10-02 18:48:36 +0200

    feat: Login and register page

---
+++ web/src/public/login.js
@@ -0,0 +1,25 @@
+const usernameElement = document.getElementById("username");
+const passwordElement = document.getElementById("password");
+
+document.getElementsByTagName("form")[0].onsubmit = async (e) => {
+	console.log("d");
+	e.preventDefault();
+
+	const res = await fetch("/api/auth/login", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			username: usernameElement.value,
+			password: passwordElement.value,
+		}),
+	});
+
+	if (res.status == 200) {
+		window.location.href = "/";
+		return;
+	}
+
+	alert("Failed to login");
+};
