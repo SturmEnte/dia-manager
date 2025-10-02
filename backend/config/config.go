1f476ff095030d76f40940298fabade857dca80d
Jonas <jonas@sturmente.de>
2025-10-02 15:29:33 +0200

    feat: Added token lifetime to config vars

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,16 +1,29 @@
 package config
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 type Config struct {
     ServerPort string
     DatabaseUri string
+    TokenLifetime int
 }
 
 func Load() *Config {
+    
+    tokenLifetimeStr := getEnv("TOKEN_LIFETIME", "5")
+    tokenLifetime, err := strconv.Atoi(tokenLifetimeStr)
+    
+    if err != nil {
+        tokenLifetime = 5 // Default
+    }
+
     return &Config{
         ServerPort: getEnv("PORT", "8369"),
         DatabaseUri: getEnv("DATABASE_URI", "postgres://test:test@localhost:5432/testdb?sslmode=disable"),
+        TokenLifetime: tokenLifetime,
     }
 }
 
