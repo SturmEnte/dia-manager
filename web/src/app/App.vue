f77cae8927ae75cd53599ce9346dbd5881827686
Jonas <jonas@sturmente.de>
2025-10-04 01:31:15 +0200

    feat: Styled header

--- web/src/app/App.vue
+++ web/src/app/App.vue
@@ -19,11 +19,16 @@ onMounted(async () => {
 <template>
 	<div>
 		<div id="header">
-			<nav>
-				<router-link to="/">Dashboard</router-link>
-				<router-link to="/catheters">Katheter</router-link>
-			</nav>
-			<div>{{ username }}</div>
+			<div id="left-h">
+				<div id="app-name">DiaManager</div>
+				<nav>
+					<router-link to="/">Dashboard</router-link>
+					<router-link to="/catheters">Katheter</router-link>
+				</nav>
+			</div>
+			<div>
+				Welcome back <span id="username">{{ username }}</span> !
+			</div>
 		</div>
 		<main>
 			<router-view />
@@ -31,4 +36,45 @@ onMounted(async () => {
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#header {
+	background: var(--col-2);
+	border-radius: var(--radius);
+	padding: var(--padding);
+
+	width: calc(100vw - 2 * var(--padding));
+
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	flex-direction: row;
+}
+
+#header #left-h {
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	flex-direction: row;
+}
+
+#header #app-name {
+	margin-right: var(--padding);
+	font-size: 1.2rem;
+}
+
+/*router-link turns into a when rendered*/
+#header nav a {
+	margin-right: calc(var(--padding) / 2);
+	text-decoration: none;
+	color: var(--col-font);
+}
+
+#header nav a.router-link-active {
+	color: var(--col-accent);
+}
+
+#username {
+	color: var(--col-accent);
+	font-style: italic;
+}
+</style>
