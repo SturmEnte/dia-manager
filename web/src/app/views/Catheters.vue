e3d19f7350065f3fb1eeb3e2a19ba48269e2562f
Jonas <jonas@sturmente.de>
2025-10-14 20:38:06 +0200

    feat: Front end create cathers interface

--- web/src/app/views/Catheters.vue
+++ web/src/app/views/Catheters.vue
@@ -9,6 +9,27 @@ const catheters = ref([]);
 onMounted(async () => {
 	catheters.value = await api.getCatheters();
 });
+
+// Create new entry
+const start = ref("");
+const end = ref("");
+
+async function createCatheter() {
+	if (!start.value) return;
+
+	const newCatheter = await api.createCatheter(start.value, end.value);
+
+	if (!newCatheter) {
+		return;
+	}
+
+	// Add new catheter to the catheters at the first position
+	catheters.value.unshift(newCatheter);
+
+	// Reset form fields
+	start.value = "";
+	end.value = "";
+}
 </script>
 
 <template>
@@ -19,7 +40,16 @@ onMounted(async () => {
 				<Catheter class="catheter" v-for="catheter in catheters" :key="catheter.id" :id="catheter.id" :started-at="catheter.startedAt" :ended-at="catheter.endedAt" />
 			</div>
 		</div>
-		<div id="create" class="window"></div>
+		<div id="create" class="window">
+			<div class="title">Neuen Katheter eintragen</div>
+			<form id="create-form" @submit.prevent="createCatheter">
+				<label for="start">Start</label>
+				<input id="start" name="start" type="datetime-local" required v-model="start" />
+				<label for="end">Ende</label>
+				<input id="end" name="end" type="datetime-local" v-model="end" :min="start || undefined" />
+				<button type="submit">Eintragen</button>
+			</form>
+		</div>
 	</div>
 </template>
 
