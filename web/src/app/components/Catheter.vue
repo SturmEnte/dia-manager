f3e13ee81de3e525dad4b2bac92b1b0d0ea8932d
Jonas <jonas@sturmente.de>
2025-10-14 17:49:22 +0200

    feat: Show usage time

--- web/src/app/components/Catheter.vue
+++ web/src/app/components/Catheter.vue
@@ -19,8 +19,10 @@ function formatDate(value) {
 
 	const date = value instanceof Date ? value : new Date(value);
 
+	// Check if the date was defined successfully
 	if (isNaN(date.getTime())) return "-";
 
+	// Format date
 	const dd = String(date.getDate()).padStart(2, "0");
 	const mm = String(date.getMonth() + 1).padStart(2, "0");
 	const yyyy = date.getFullYear();
@@ -28,6 +30,32 @@ function formatDate(value) {
 	const min = String(date.getMinutes()).padStart(2, "0");
 
 	return `${dd}.${mm}.${yyyy} ${hh}:${min}`;
+}
+
+function formatDuration(start, end) {
+	if (!start) return "-";
+	const startDate = start instanceof Date ? start : new Date(start);
+	const endDate = end ? (end instanceof Date ? end : new Date(end)) : new Date();
+
+	// Check if the date was defined successfully
+	if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) return "-";
+
+	// Time difference between start and end in ms
+	let diffMs = endDate.getTime() - startDate.getTime();
+	if (diffMs < 0) return "-";
+
+	// Format the difference from ms to more human readble format (days, hours, minutes)
+	const totalMinutes = Math.floor(diffMs / 60000);
+	const days = Math.floor(totalMinutes / (60 * 24));
+	const hours = Math.floor((totalMinutes % (60 * 24)) / 60);
+	const minutes = totalMinutes % 60;
+
+	const parts = [];
+	if (days) parts.push(`${days}d`);
+	if (hours) parts.push(`${hours}h`);
+	parts.push(`${minutes}m`);
+
+	return parts.join(" ");
 }
 </script>
 
@@ -38,7 +66,7 @@ function formatDate(value) {
 			<div><span class="attr-title">Beendet:</span> {{ formatDate(props.endedAt) }}</div>
 		</div>
 		<div class="side">
-			<div><span class="attr-title">Tragedauer:</span> {{ null }}</div>
+			<div><span class="attr-title">Tragedauer:</span> {{ formatDuration(props.startedAt, props.endedAt) }}</div>
 			<div><span class="attr-title">Wechselgrund:</span> {{ null }}</div>
 		</div>
 	</div>
